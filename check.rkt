2fe25ab0206f42f6ed66f271e9c6dca391e13877
Rick van de Loo <rickvandeloo@gmail.com>
2021-08-21 18:00:38 +0200

    implement n-bit-ripple-carry-adder

--- check.rkt
+++ check.rkt
@@ -6,8 +6,96 @@
 
 ; This file contains assertions to verify if the circuit simulation is working as expected
 
-(define (check-simulation)
-    (begin
+(define (check-ripple-carry ripple-carry-circuit)
+  (begin
+      (unless (equal? (list 0 0 0 0 0)
+		      (ripple-carry-circuit 
+			0 0 0 0 ; A1 - A4
+			0 0 0 0 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 0 output for this input"))
+      (unless (equal? (list 0 0 0 0 1)
+		      (ripple-carry-circuit 
+			0 0 0 0 ; A1 - A4
+			0 0 0 0 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 1 output for this input"))
+      (unless (equal? (list 0 1 0 0 0)
+		      (ripple-carry-circuit 
+			0 1 0 0 ; A1 - A4
+			0 1 0 0 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 8 output for this input"))
+      (unless (equal? (list 0 1 0 0 1)
+		      (ripple-carry-circuit 
+			0 1 0 0 ; A1 - A4
+			0 1 0 0 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 9 output for this input"))
+      (unless (equal? (list 1 0 0 0 0)
+		      (ripple-carry-circuit 
+			1 0 0 0 ; A1 - A4
+			1 0 0 0 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 0 output for this input with carry bit"))
+      (unless (equal? (list 1 0 0 0 1)
+		      (ripple-carry-circuit 
+			1 0 0 0 ; A1 - A4
+			1 0 0 0 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 1 output for this input with carry bit"))
+      (unless (equal? (list 1 0 1 0 0)
+		      (ripple-carry-circuit 
+			1 0 1 0 ; A1 - A4
+			1 0 1 0 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 4 output for this input with carry bit"))
+      (unless (equal? (list 1 0 1 0 1)
+		      (ripple-carry-circuit 
+			1 0 1 0 ; A1 - A4
+			1 0 1 0 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 5 output for this input with carry bit"))
+      (unless (equal? (list 1 1 0 0 0)
+		      (ripple-carry-circuit 
+			1 1 0 0 ; A1 - A4
+			1 1 0 0 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 8 output for this input with carry bit"))
+      (unless (equal? (list 1 1 0 0 1)
+		      (ripple-carry-circuit 
+			1 1 0 0 ; A1 - A4
+			1 1 0 0 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 9 output for this input with carry bit"))
+      (unless (equal? (list 1 1 1 0 0)
+		      (ripple-carry-circuit 
+			1 1 1 0 ; A1 - A4
+			1 1 1 0 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 12 output for this input with carry bit"))
+      (unless (equal? (list 1 1 1 0 1)
+		      (ripple-carry-circuit 
+			1 1 1 0 ; A1 - A4
+			1 1 1 0 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 13 output for this input with carry bit"))
+      (unless (equal? (list 1 1 1 1 0)
+		      (ripple-carry-circuit 
+			1 1 1 1 ; A1 - A4
+			1 1 1 1 ; B1 - B4
+			0       ; Cin
+			)) (error "Four bit ripple carry adder expected 14 output for this input with carry bit"))
+      (unless (equal? (list 1 1 1 1 1)
+		      (ripple-carry-circuit 
+			1 1 1 1 ; A1 - A4
+			1 1 1 1 ; B1 - B4
+			1       ; Cin
+			)) (error "Four bit ripple carry adder expected 15 output for this input with carry bit"))))
+
+
+(define (check-bits)
+  (begin
       (unless (= 0 (wire-or 0 0)) (error "OR with input 0 0 should output 0"))
       (unless (= 1 (wire-or 1 0)) (error "OR with input 1 0 should output 1"))
       (unless (= 1 (wire-or 0 1)) (error "OR with input 0 1 should output 1"))
@@ -52,4 +140,24 @@
       (unless (= 1 (cdr (wire-full-adder 0 1 1))) (error "Full adder with input 0 1 1 should have cout 1"))
       (unless (= 1 (cdr (wire-full-adder 1 0 1))) (error "Full adder with input 1 0 1 should have cout 1"))
       (unless (= 1 (cdr (wire-full-adder 1 1 1))) (error "Full adder with input 1 1 1 should have cout 1"))
+
+      (check-ripple-carry wire-four-bit-ripple-carry-adder)
+      (check-ripple-carry wire-n-bit-ripple-carry-adder)
 ))
+
+(define (check-numbers)
+  (define (check-add a b)
+    (let ([added (ripple-carry-adder a b)])
+      (unless 
+	(= (+ a b) added)
+	(error 
+	  (format "Expected ~a + ~a = ~a, but the ripple-carry-adder did ~a + ~a = ~a" 
+		  a b (+ a b) a b added)))
+      added))
+  (map check-add (range (expt 2 10)) (range (expt 2 10))))
+
+(define (check-simulation)
+    (check-bits)
+    (check-numbers)
+    (displayln "Passed all checks!")
+)
--- circuits.rkt
+++ circuits.rkt
@@ -9,18 +9,40 @@
 (provide wire-xor)
 (provide wire-half-adder)
 (provide wire-full-adder)
+(provide wire-four-bit-ripple-carry-adder)
+(provide wire-n-bit-ripple-carry-adder)
+(provide ripple-carry-adder)
 
-(define (half-adder-gate a b s c)
+(define (half-adder-circuit a b s c)
   (and-gate a b c)
   (xor-gate a b s))
 
-(define (full-adder-gate a b cin s cout)
+(define (full-adder-circuit a b cin s cout)
   (let ([wire-co1 (make-wire)]
 	[wire-co2 (make-wire)]
 	[wire-so1 (make-wire)])
-  (half-adder-gate a b wire-so1 wire-co2)
-  (half-adder-gate cin wire-so1 s wire-co1)
+  (half-adder-circuit a b wire-so1 wire-co2)
+  (half-adder-circuit cin wire-so1 s wire-co1)
   (or-gate wire-co1 wire-co2 cout)))
+
+(define (four-bit-ripple-carry-adder a1 a2 a3 a4 b1 b2 b3 b4 c1 c2 c3 c4 cin cout)
+  (let ([wire-cout2 (make-wire)]
+	[wire-cout3 (make-wire)]
+	[wire-cout4 (make-wire)])
+    (full-adder-circuit a4 b4 cin c4 wire-cout2)
+    (full-adder-circuit a3 b3 wire-cout2 c3 wire-cout3)
+    (full-adder-circuit a2 b2 wire-cout3 c2 wire-cout4)
+    (full-adder-circuit a1 b1 wire-cout4 c1 cout)))
+
+(define (n-bit-ripple-carry-adder a-list b-list cin)
+  (define (n-bit-ripple-carry-adder-iter a-list b-list s-list cin cout)
+    (let ([wire-sum (make-wire)])
+      (if (null? a-list)
+	(cons cin s-list)
+	(begin
+	  (full-adder-circuit (car a-list) (car b-list) cin wire-sum cout)
+	  (n-bit-ripple-carry-adder-iter (cdr a-list) (cdr b-list) (cons wire-sum s-list) cout (make-wire))))))
+  (n-bit-ripple-carry-adder-iter a-list b-list '() cin (make-wire)))
 
 (define (wire-or a b)
   (let ([wire-a (make-wire)]
@@ -67,7 +89,7 @@
 	[wire-c (make-wire)])
     (set-signal! wire-a a)
     (set-signal! wire-b b)
-    (half-adder-gate wire-a wire-b wire-s wire-c)
+    (half-adder-circuit wire-a wire-b wire-s wire-c)
     (propagate)
     (cons (get-signal wire-s) (get-signal wire-c))))
 
@@ -80,6 +102,92 @@
     (set-signal! wire-a a)
     (set-signal! wire-b b)
     (set-signal! wire-cin c)
-    (full-adder-gate wire-a wire-b wire-cin wire-s wire-cout)
+    (full-adder-circuit wire-a wire-b wire-cin wire-s wire-cout)
     (propagate)
     (cons (get-signal wire-s) (get-signal wire-cout))))
+
+(define (wire-four-bit-ripple-carry-adder a1 a2 a3 a4 b1 b2 b3 b4 cin)
+  (let ([wire-a1 (make-wire)]
+	[wire-a2 (make-wire)]
+	[wire-a3 (make-wire)]
+	[wire-a4 (make-wire)]
+	[wire-b1 (make-wire)]
+	[wire-b2 (make-wire)]
+	[wire-b3 (make-wire)]
+	[wire-b4 (make-wire)]
+	[wire-c1 (make-wire)]
+	[wire-c2 (make-wire)]
+	[wire-c3 (make-wire)]
+	[wire-c4 (make-wire)]
+	[wire-cin (make-wire)]
+	[wire-cout (make-wire)])
+    (set-signal! wire-a1 a1)
+    (set-signal! wire-a2 a2)
+    (set-signal! wire-a3 a3)
+    (set-signal! wire-a4 a4)
+    (set-signal! wire-b1 b1)
+    (set-signal! wire-b2 b2)
+    (set-signal! wire-b3 b3)
+    (set-signal! wire-b4 b4)
+    (set-signal! wire-cin cin)
+    (four-bit-ripple-carry-adder 
+      wire-a1 wire-a2 wire-a3 wire-a4 
+      wire-b1 wire-b2 wire-b3 wire-b4
+      wire-c1 wire-c2 wire-c3 wire-c4
+      wire-cin wire-cout)
+    (propagate)
+    (map get-signal (list wire-cout wire-c1 wire-c2 wire-c3 wire-c4))))
+
+(define (list-wire-n-bit-ripple-carry-adder a-list b-list cin)
+  (let ([s-list (n-bit-ripple-carry-adder a-list b-list cin)])
+    (propagate)
+    (map get-signal s-list)))
+
+(define (wire-n-bit-ripple-carry-adder a1 a2 a3 a4 b1 b2 b3 b4 cin)
+  (let ([wire-a1 (make-wire)]
+	[wire-a2 (make-wire)]
+	[wire-a3 (make-wire)]
+	[wire-a4 (make-wire)]
+	[wire-b1 (make-wire)]
+	[wire-b2 (make-wire)]
+	[wire-b3 (make-wire)]
+	[wire-b4 (make-wire)]
+	[wire-cin (make-wire)])
+    (set-signal! wire-a1 a1)
+    (set-signal! wire-a2 a2)
+    (set-signal! wire-a3 a3)
+    (set-signal! wire-a4 a4)
+    (set-signal! wire-b1 b1)
+    (set-signal! wire-b2 b2)
+    (set-signal! wire-b3 b3)
+    (set-signal! wire-b4 b4)
+    (set-signal! wire-cin cin)
+    (list-wire-n-bit-ripple-carry-adder 
+      (list wire-a4 wire-a3 wire-a2 wire-a1)
+      (list wire-b4 wire-b3 wire-b2 wire-b1)
+      wire-cin)))
+
+(define (ripple-carry-adder a b)
+  (begin
+    (define (get-binary-list integer)
+      (map (lambda x (if (equal? x '(49)) 1 0))
+	   (map char->integer (string->list (format "~b" integer)))))
+    (define (pad-list list-in len)
+      (if (< (length list-in) len)
+	(pad-list (cons 0 list-in) len)
+	list-in))
+    (define (create-wire v)
+      (let ([wire (make-wire)])
+	(set-signal! wire v)
+	wire))
+    (define list-a (get-binary-list a))
+    (define list-b (get-binary-list b))
+    (define max-len (+ (max (length list-a) (length list-b)) 1))
+    (define padded-list-a (pad-list list-a max-len))
+    (define padded-list-b (pad-list list-b max-len))
+    (define s-list
+      (list-wire-n-bit-ripple-carry-adder
+	(map create-wire (reverse padded-list-a))
+	(map create-wire (reverse padded-list-b))
+	(create-wire 0)))
+    (string->number (string-join (map number->string s-list) "") 2)))
